ae523b0488938cc5c5baf7758031fe2c6a3195f1
KJThoma <kyle.j.thoma@gmail.com>
2022-06-25 20:31:20 -0400

    Updating to use API for advice, coinflips, and rolls via random

--- budbot.py
+++ budbot.py
@@ -1,40 +1,26 @@
 import discord
 import random
+import requests
+import json
 
-TOKEN = 'your bot token here'
+TOKEN = 'your token here'
 
 client = discord.Client()
 
-# fortunes cookie quotes the bot will reply with when messaged for !advice
-fortunes = ['Stop wishing. Start doing.' ,
-            'if love someone a lot tell it before its too late',
-            'For success today look first to yourself.',
-            'An unexpected aquaintance will resurface.' ,
-            'You are given the chance to take part in an exciting adventure.' ,
-            'Soon, a visitor shall delight you.' ,
-            'You are very talented in many ways.' ,
-            'The eyes believe themselves; the ears believe other people.' ,
-            'Your difficult work will get payoff today.' ,
-            'All the water in the world cant sink a ship unless it gets inside.',
-            'Good news from afar may bring you a welcome visitor.',
-            'May you someday be carbon neutral.',
-            'Your tongue is your ambassador',
-            'Life is too short to waste time hating anyone.',
-            'He who throws dirt is losing ground.',
-            'Make a wise choice everyday.',
-            'Failure is not defeat until you stop trying.',
-            'Keep true to the dreams of your youth. ',
-            'Soon you will be sitting on top of the world.',
-            'Be calm when confronting an emergency crisis.',
-            'Land is always on the mind of a flying bird.',
-            'Trust your intuition.']
+# function for advice API
+def get_advice():
+    response = requests.get('https://api.adviceslip.com/advice')
+    json_data = json.loads(response.text)
+    quote = json_data['slip']
+    return(quote['advice'])
 
-# bot login message
+# coin flip results
+coin_flip = ['heads', 'tails']
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-# recording the chat messages, needed for responses and advice
 @client.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
@@ -42,13 +28,12 @@ async def on_message(message):
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
 
-# chat messages for the bot to respond too, ignoring the bot itself
     if message.author == client.user:
         return
-    if message.channel.name == 'your discord channel here':
+    if message.channel.name == 'your channel here':
         # hello responses
         if user_message.lower() == 'hey bud':
-            await message.channel.send(f'Hey {username}!')
+            await message.channel.send(f'Hey {username}!'))
             return
         # goodbye responses
         elif user_message.lower() == 'bye':
@@ -56,7 +41,17 @@ async def on_message(message):
             return
         # give fortune cookie advice
         elif user_message.lower() == '!advice':
-            await message.channel.send(random.choice(fortunes))
+            advice = get_advice()
+            await message.channel.send(advice)
+            return
+        # random number generator
+        elif user_message.lower() == '!roll':
+            random_number = random.randint(1, 100)
+            await message.channel.send(random_number)
+            return
+        # coin flip
+        elif user_message.lower() == '!coinflip':
+            await message.channel.send(random.choice(coin_flip))
             return
 
 client.run(TOKEN)
